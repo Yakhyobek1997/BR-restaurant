965b0d806909df38e33a14cf9f76b6045d314ad3
Yakhyobek1997 <yahyobek19979797@gmail.com>
2025-04-09 02:03:50 +0900

    n

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,24 +1,59 @@
 console.log("Signup frontend javascript file");
 
+$(function () {
+  const fileTarget = $(".file-box .upload-hidden");
+  let filename;
 
-$(function (){})
+  fileTarget.on("change", function () {
+    if (window.FileReader) {
+      const uploadFile = $(this)[0].files[0];
+      console.log(uploadFile);
+      const fileType = uploadFile.type;
+      console.log(fileType);
+
+      const validImageType = ["image/jpg", "image/jpeg", "image/png"];
+      if (!validImageType.includes(fileType)) {
+        alert("Please insert only jpg, jpeg, png files onlny");
+      } else {
+        if (uploadFile) {
+          console.log(URL.createObjectURL(uploadFile));
+          $(".upload-img-frame")
+            .attr("src", URL.createObjectURL(uploadFile))
+            .addClass("success");
+        }
+        filename = $(this)[0].files[0].name;
+      }
+      $(this).siblings(".upload-name").val(filename);
+    }
+  });
+});
 
 function validateSignupForm() {
-    
-    const memberNick = $(".member-nick").val()
-    const memberPhone = $(".member-phone").val()
-    const memberPassword = $(".member-password").val()
-    const confirmPassword = $(".confirm-password").val()
-    
-    if (memberNick === "" || memberPhone === "" || memberPassword === ""
-        || confirmPassword === ""
-     ) {
-        alert("Please fill in all fields!")
-        return false
-     }
+  const memberNick = $(".member-nick").val();
+  const memberPhone = $(".member-phone").val();
+  const memberPassword = $(".member-password").val();
+  const confirmPassword = $(".confirm-password").val();
 
-     if(memberPassword !== confirmPassword) {
-        alert("Passwords do not match, please cheeck again!")
+  if (
+    memberNick === "" ||
+    memberPhone === "" ||
+    memberPassword === "" ||
+    confirmPassword === ""
+  ) {
+    alert("Please fill in all fields!");
+    return false;
+  }
+
+  if (memberPassword !== confirmPassword) {
+    alert("Passwords do not match, please cheeck again!");
+    return false;
+  }
+
+  const memberImage = $(".member-image").get(0).files[0]
+    ? $(".member-image").get(0).files[0].name
+    : null;
+    if( !memberImage) {
+        alert("Please upload an image!")
         return false
-     }
+    }
 }
