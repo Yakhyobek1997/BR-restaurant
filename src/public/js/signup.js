9be860744b1c1a8334e342dd23885c675045ba05
Yakhyobek1997 <yahyobek19979797@gmail.com>
2025-04-11 00:08:32 +0900

    Fix: admin project ref

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -29,10 +29,10 @@ $(function () {
 });
 
 function validateSignupForm() {
-  const memberNick = $(".member-nick").val();
-  const memberPhone = $(".member-phone").val();
-  const memberPassword = $(".member-password").val();
-  const confirmPassword = $(".confirm-password").val();
+  const memberNick = $(".member-nick").val(),
+    memberPhone = $(".member-phone").val(),
+    memberPassword = $(".member-password").val(),
+    confirmPassword = $(".confirm-password").val();
 
   if (
     memberNick === "" ||
@@ -52,8 +52,8 @@ function validateSignupForm() {
   const memberImage = $(".member-image").get(0).files[0]
     ? $(".member-image").get(0).files[0].name
     : null;
-    if( !memberImage) {
-        alert("Please upload an image!")
-        return false
-    }
+  if (!memberImage) {
+    alert("Please upload an image!");
+    return false;
+  }
 }
